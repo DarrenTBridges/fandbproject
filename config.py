dabe25ab19110bb7c33edcfa2d67e6c9875c1af5
Darren Bridges <darrenbridges@Darrens-Air.lan>
2020-09-13 21:42:40 +0100

    Initial commit v0.146

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        "postgres://iqzepczsapjjoz:3cef1cf46aacdd8f5bc36c7e5f527c5f97157c8660001380b45cf91cf7547919@ec2-52-31-233-101.eu-west-1.compute.amazonaws.com:5432/d1hbdtja7o8kvo"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
