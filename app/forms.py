ad2e33e2482f6a3fe1980522de39d68870f1e1bd
Darren Bridges <darrenbridges@Darrens-Air.lan>
2020-09-13 21:08:11 +0100

    initial commit v0.14

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, PasswordField, BooleanField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
+from app.models import User 
 
 class LoginForm(FlaskForm):
-    email = StringField('Username', validators=[DataRequired(), Email()])
+    email = StringField('Email address', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    firstname = StringField('First name', validators=[DataRequired()])
+    lastname = StringField('Last name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Confirm password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
